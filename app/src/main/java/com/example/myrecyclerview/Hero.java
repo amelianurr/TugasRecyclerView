cd2214f7cd7f6585a349e2c0598d56ba9831170e
amelianurr <amelianur523@gmail.com>
2019-09-07 15:56:43 +0700

    first

---
+++ app/src/main/java/com/example/myrecyclerview/Hero.java
@@ -0,0 +1,32 @@
+package com.example.myrecyclerview;
+
+public class Hero {
+    private String name;
+    private String detail;
+    private int photo;
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+}
